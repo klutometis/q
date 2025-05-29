c88df1a1a110b7f69a46bd3a848f1cc15029b91e
Peter Danenberg <pcd@roxygen.org>
2025-05-28 19:34:31 -0700

    feat: Add --type flag for elvi engine
    This commit introduces a new `--type` flag (short `-t`)
    to specify an optional type when using a preset search engine
    with `--engine`. This allows for more specific searches,
    e.g., `q -e <engine> -t code <query>`.

--- q.sh
+++ q.sh
@@ -4,25 +4,30 @@ SCRIPT_DIR="$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" && pwd)"
 . "${SCRIPT_DIR}/lib/shflags/shflags"
 . "${SCRIPT_DIR}/lib/bashlog/log.sh"
 
-# Flags
+# Define flags
 DEFINE_string 'engine' '' 'Preset search engine (elvi)' 'e'
+DEFINE_string 'type' '' 'Optional type passed to elvi (e.g. code, repo)' 't'
 DEFINE_boolean 'debug' false 'Enable debug logging' 'd'
+
 FLAGS "$@" || exit $?
 eval set -- "${FLAGS_ARGV}"
+
 [ "${FLAGS_debug}" -eq "${FLAGS_TRUE}" ] && DEBUG=1 || DEBUG=0
 
 main() {
   log debug "Starting q.sh"
   local elvi_list input engine query
+
+  # Get list of elvi
   elvi_list=$(sr -elvi | awk 'NR > 1 {print $1}')
 
+  # Case: --engine provided (curried search)
   if [ -n "$FLAGS_engine" ]; then
     engine="$FLAGS_engine"
     if echo "$elvi_list" | grep -qx "$engine"; then
       query=$(echo "" | dmenu -p "Search $engine:")
-      [ -z "$query" ] && log info "No query; exiting" && exit 0
-      log info "Launching: sr $engine '$query'"
-      sr "$engine" "$query"
+      log info "Launching: sr $engine $([ -n "$FLAGS_type" ] && echo "-t=$FLAGS_type") $query"
+      sr "$engine" ${FLAGS_type:+-t="$FLAGS_type"} $query
     else
       log error "Unknown engine: '$engine'"
       exit 1
@@ -30,7 +35,7 @@ main() {
     exit 0
   fi
 
-  # Use dmenu with trailing-space elvi list
+  # Case: generic mode via dmenu
   input=$(echo "$elvi_list" | sed 's/$/ /' | dmenu -p "Search:")
   [ -z "$input" ] && log info "No input; exiting" && exit 0
 
@@ -38,7 +43,7 @@ main() {
   query=$(echo "$input" | cut -d' ' -f2-)
 
   if echo "$elvi_list" | grep -qx "$engine"; then
-    log info "Launching: sr $engine '$query'"
+    log info "Launching: sr $engine $query"
     sr "$engine" $query
   else
     log error "Unknown engine: '$engine'"
